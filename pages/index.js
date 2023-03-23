894d295c9d6b30101896bc5bf56d606e8e979a47
AgataHerbst <125795402+AgataHerbst@users.noreply.github.com>
2023-03-23 21:31:42 +0300

    изменен обработчик ошибок

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,13 @@
 import Header from '../components/Header';
 import Tbody from '../components/Tbody';
 import s from './index.module.css';
-import {ErrorBoundary} from 'react-error-boundary';
-import {ErrorFallBack} from '../components/ErrorFallBack';
+
 
 function App() {
- return <>
+  return <>
     <Header />
     <table className={s.app}>
-    <ErrorBoundary
-     FallbackComponent={ErrorFallBack}>
       <Tbody />
-    </ErrorBoundary>
     </table>
   </>
 }
